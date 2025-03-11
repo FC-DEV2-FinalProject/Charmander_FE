8fd3bcefb296c6f0873629faa7b740f5e4169125
park-chan-hui <fhfh54321@motionimaging.net>
2025-03-11 18:40:50 +0900

    feat: 프로젝트 생성-기사 입력 페이지 초기 퍼블리싱(실시간 저장 로직 추가 필요)

--- src/routes/_projectSideBarLayout/$project/article/index.tsx
+++ src/routes/_projectSideBarLayout/$project/article/index.tsx
@@ -1,4 +1,8 @@
+import { useDebounce } from '@/hook/useDebounce';
+import theme from '@/styles/theme';
 import { createFileRoute } from '@tanstack/react-router';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 export const Route = createFileRoute(
   '/_projectSideBarLayout/$project/article/'
@@ -7,5 +11,50 @@ export const Route = createFileRoute(
 });
 
 function RouteComponent() {
-  return <div>기사 입력 창입니다다!</div>;
+  const [article, setArticle] = useState('');
+
+  const debouncedArticle = useDebounce(article, 1000);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setArticle(e.target.value);
+  };
+
+  useEffect(() => {
+    if (debouncedArticle) {
+      // console.log(debouncedArticle);
+      //추후 저장 로직으로 변경 예정
+    }
+  }, [debouncedArticle]);
+
+  return (
+    <S.ArticleBox>
+      <textarea
+        placeholder="기사를 입력해주세요"
+        value={article}
+        onChange={handleChange}
+      />
+    </S.ArticleBox>
+  );
 }
+
+const S = {
+  ArticleBox: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: ${theme.colors.primary};
+    border: 1px solid ${theme.colors.black};
+    border-radius: ${theme.radius.medium};
+    padding: ${theme.spacing.md};
+    textarea {
+      width: 100%;
+      height: 100%;
+      border-radius: ${theme.radius.medium};
+      padding: ${theme.spacing.md};
+      box-sizing: border-box;
+      font: inherit;
+    }
+  `,
+};
