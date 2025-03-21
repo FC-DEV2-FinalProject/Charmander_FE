c031a3a3efc2f9ea3d09d75cafea47bfca248d6e
chajaesik01 <ckwotlr@dongguk.edu>
2025-03-21 22:05:42 +0900

    refactor: 백엔드분들의 의견 반영 (#34)

--- src/routes/auth/callback/google/index.tsx
+++ src/routes/auth/callback/google/index.tsx
@@ -26,7 +26,10 @@ function RouteComponent() {
           return;
         }
 
-        const response = await axios.post('/api/auth/google', { code });
+        const response = await axios.post(
+          import.meta.env.VITE_OAUTH_API_URL + '/google',
+          { code }
+        );
 
         localStorage.setItem('userProfile', JSON.stringify(response.data.user));
         localStorage.setItem('token', response.data.token);
