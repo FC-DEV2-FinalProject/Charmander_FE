6e13d03f054624f4b6f97ee340be5806bf4613ae
chajaesik01 <ckwotlr@dongguk.edu>
2025-03-23 14:56:46 +0900

    refactor: api 함수를 수정합니다 (#38)

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,21 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  const apiUrl = env.VITE_API_URL;
-  
-  return {
-    plugins: [
-      react(),
-      TanStackRouterVite(),
-      svgr({
-        // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include.
-        include: '**/*.svg?react',
-      }),
+export default defineConfig({
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    svgr({
+      include: '**/*.svg?react',
+    }),
+  ],
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: 'node_modules', replacement: '/node_modules' },
     ],
-    resolve: {
-      alias: [
-        { find: '@', replacement: '/src' },
-        { find: 'node_modules', replacement: '/node_modules' },
-      ],
-    },
-    server: {
-      proxy: {
-        '/api': {
-          target: apiUrl,
-          changeOrigin: true,
-          secure: false,
-        }
-      }
-    }
-  };
+  },
 });
