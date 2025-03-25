1ba145ab2e3310e4d0d2451235125bf086e8bd90
park-chan-hui <fhfh54321@motionimaging.net>
2025-03-25 16:55:42 +0900

    refactor: login api에 선언 되어 있는 api 함수로 변경

--- src/api/project/api.ts
+++ src/api/project/api.ts
@@ -1,20 +1,12 @@
-import axios from 'axios';
-
-const API_BASE_URL = '/'; // 실제 API 주소로 변경
-
-const api = axios.create({
-  baseURL: API_BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+import api from '../login/api';
 
 export const fetchProjects = async (projectId: string) => {
   try {
     const response = await api.get(`/api/v1/projects/${projectId}`);
     return response.data;
   } catch (error) {
-    alert(`Error fetching projects:, ${error}`);
+    // eslint-disable-next-line no-console
+    console.error('Error fetching projects:', error);
     throw error;
   }
 };
--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -2,19 +2,22 @@ import { http, HttpResponse } from 'msw';
 import dummyData from './dummy.json';
 
 export const handlers = [
-  http.get('/api/v1/projects/:id', async ({ params }) => {
-    const { id } = params;
+  http.get(
+    `${import.meta.env.VITE_API_URL}/api/v1/projects/:id`,
+    async ({ params }) => {
+      const { id } = params;
 
-    const project = dummyData.projects.find((p) => p.id === Number(id));
+      const project = dummyData.projects.find((p) => p.id === Number(id));
 
-    //임의로 오류 코드 404 반환하도록 설정했음
-    if (!project) {
-      return HttpResponse.json(
-        { message: 'Project not found' },
-        { status: 404 }
-      );
+      //임의로 오류 코드 404 반환하도록 설정했음
+      if (!project) {
+        return HttpResponse.json(
+          { message: 'Project not found' },
+          { status: 404 }
+        );
+      }
+
+      return HttpResponse.json(project, { status: 200 });
     }
-
-    return HttpResponse.json(project, { status: 200 });
-  }),
+  ),
 ];
