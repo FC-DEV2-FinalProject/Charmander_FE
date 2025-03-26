25a140e33c6255fee9d843fcf180d2957922f010
park-chan-hui <fhfh54321@motionimaging.net>
2025-03-26 15:19:19 +0900

    feat: 에디터 api 작업에 필요한 더미 데이터 생성 및 일부 api 함수 생성

--- src/api/project/api.ts
+++ src/api/project/api.ts
@@ -10,3 +10,43 @@ export const fetchProjects = async (projectId: string) => {
     throw error;
   }
 };
+
+export const postProjectTitle = async (projectId: string, title: string) => {
+  try {
+    const response = await api.post(
+      `/api/v1/projects/${projectId}/updateTitle`,
+      {
+        name: title,
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('프로젝트 제목 업데이트 실패:', error);
+    throw error;
+  }
+};
+
+export const suggestArticle = async (article: string) => {
+  try {
+    const response = await api.post('/api/v1/templates/suggest', { article });
+    return response.data;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error suggesting article:', error);
+    throw error;
+  }
+};
+export const updateArticle = async (projectId: string, article: string) => {
+  try {
+    const response = await api.post(`/api/v1/article/${projectId}/update`, {
+      article,
+    });
+    return response.data;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error updating article:', error);
+    throw error;
+  }
+};
