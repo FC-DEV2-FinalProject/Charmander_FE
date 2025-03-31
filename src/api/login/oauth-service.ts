7afd09417747abb0f8f17e1f515caa944ae514ef
chajaesik01 <ckwotlr@dongguk.edu>
2025-03-31 11:12:57 +0900

    refactor: state값을 임의로 생성해서 api 요청 (#91)

--- src/api/login/oauth-service.ts
+++ src/api/login/oauth-service.ts
@@ -5,18 +5,17 @@ export const getAuthorizationUrl = (
   provider: OAuthProvider,
   state?: string
 ): string => {
+  if (!state) {
+    state = crypto.randomUUID() || Math.random().toString(36).substring(2, 15);
+  }
+
   const params = new URLSearchParams({
     client_id: provider.clientId,
     redirect_uri: provider.redirectUri,
     response_type: 'code',
     scope: provider.scope,
+    state: state,
   });
-
-  if (state) {
-    params.append('state', state);
-  } else {
-    state = '';
-  }
 
   return `${provider.authUrl}?${params.toString()}`;
 };
