04041c0d2306a02f58b9ece0df14e96ac2935226
박찬희 <fhfh54321@motionimaging.net>
2025-04-01 17:53:28 +0900

    fix: 대사 수정 시 debounce 미적용 문제 해결

--- src/routes/_projectSideBarLayout/$project/script/index.tsx
+++ src/routes/_projectSideBarLayout/$project/script/index.tsx
@@ -14,9 +14,6 @@ import {
 } from '@/api/project/api';
 import { useDebounce } from '@/hook/useDebounce';
 import VideoViewPortComponent from '@/components/project-editor/scriptVeiwPort';
-// import ShortScriptTemplate from '@/assets/projectIcon/scriptTemplateShort.svg?react';
-// import LongScriptTemplate from '@/assets/projectIcon/scriptTemplateLong.svg?react';
-// import TransparentBackground from '@/assets/projectIcon/transparentBackground.svg?react';
 
 export const Route = createFileRoute('/_projectSideBarLayout/$project/script/')(
   {
@@ -25,45 +22,19 @@ export const Route = createFileRoute('/_projectSideBarLayout/$project/script/')(
 );
 
 function RouteComponent() {
-  // const fonts = [
-  //   'NexonLv2',
-  //   'Noto Sans KR',
-  //   'Nanum Gothic',
-  //   'Nanum Myeongjo',
-  //   'IBM Plex Sans KR',
-  //   'Gmarket Sans',
-  //   'Spoqa Han Sans',
-  //   'Arial',
-  //   'Times New Roman',
-  //   'Verdana',
-  // ];
-  // const weights = ['얇게', '중간', '굵게'];
-  // const sizes = Object.values(theme.fontSizes);
-  // const backgrounds = ['short', 'long', 'transparent'];
-
-  // const [backgroundStyle, setBackgroundStyle] = useState('short');
-  // const [promptValue, setPromptValue] = useState('');
   const { aspectRatio } = useAspectRatioStore();
   const { projectData, updateTranscripts } = useProjectEditorStore();
   const [termValue, setTermValue] = useState(`0.5`);
-  const [subtitles, setSubtitles] = useState<Transcript[]>(
-    projectData?.scenes[0].transcripts || [
-      {
-        id: 1,
-        sceneId: projectData?.scenes[0]?.id ?? 1,
-        text: '대사를 입력해주세요.',
-        property: {
-          speed: 1,
-          postDelay: 500,
-        },
-        createdAt: Date.now().toString(),
-        updatedAt: Date.now().toString(),
-      },
-    ]
-  );
+  const [subtitles, setSubtitles] = useState<Transcript[]>([]);
   const [selectedSubtitle, setSelectedSubtitle] = useState<Transcript | null>(
     null
   );
+
+  useEffect(() => {
+    if (projectData) {
+      setSubtitles(projectData.scenes[0].transcripts);
+    }
+  }, [projectData]);
 
   const debouncedSubtitles = useDebounce(subtitles, 1000);
 
@@ -96,13 +67,6 @@ function RouteComponent() {
         subtitle.id === id ? { ...subtitle, [field]: value } : subtitle
       )
     );
-
-    const existingSubtitle = projectData.scenes[0].transcripts.find(
-      (t) => t.id === id
-    );
-    if (existingSubtitle && existingSubtitle[field] === value) return;
-
-    updateTranscripts(projectData.scenes[0].id, id, { [field]: value });
   };
 
   const deleteSubtitle = async (id: number) => {
@@ -157,11 +121,10 @@ function RouteComponent() {
     }
   };
 
-  // subtitles 업데이트 시 store에 반영
   useEffect(() => {
     if (!projectData) return;
 
-    subtitles.forEach((subtitle) => {
+    debouncedSubtitles.forEach((subtitle) => {
       const existingSubtitle = projectData.scenes[0].transcripts.find(
         (t) => t.id === subtitle.id
       );
@@ -173,30 +136,28 @@ function RouteComponent() {
         updateTranscripts(projectData.scenes[0].id, subtitle.id, subtitle);
       }
     });
-  }, [subtitles, updateTranscripts, projectData]);
+  }, [debouncedSubtitles, updateTranscripts, projectData]);
 
   useEffect(() => {
     if (!projectData) return;
-
     const patchUpdatedSubtitles = async () => {
       try {
-        const originalTranscripts = projectData.scenes[0].transcripts ?? [];
+        const original = projectData.scenes[0].transcripts ?? [];
+        const modified = (debouncedSubtitles ?? []).filter((subtitle) => {
+          const orig = original.find((t) => t.id === subtitle.id);
+          if (orig) {
+            const isDifferent =
+              orig.text !== subtitle.text ||
+              orig.property.speed !== subtitle.property.speed ||
+              orig.property.postDelay !== subtitle.property.postDelay;
 
-        const modifiedSubtitles = (debouncedSubtitles ?? []).filter(
-          (subtitle) => {
-            const original = originalTranscripts.find(
-              (t) => t.id === subtitle.id
-            );
-            return (
-              original && JSON.stringify(original) !== JSON.stringify(subtitle)
-            );
+            return isDifferent;
           }
-        );
-
-        if (modifiedSubtitles.length === 0) return;
-
+          return false;
+        });
+        if (modified.length === 0) return;
         await Promise.all(
-          modifiedSubtitles.map((subtitle) =>
+          modified.map((subtitle) =>
             patchTranscript(
               projectData.id,
               projectData.scenes[0].id,
@@ -210,16 +171,9 @@ function RouteComponent() {
         console.error('자막 업데이트 실패:', error);
       }
     };
-
     patchUpdatedSubtitles();
   }, [debouncedSubtitles, projectData]);
-  // const selectedSubtitle = subtitles.find(
-  //   (subtitle) => subtitle.id === selectedSubtitleId
-  // );
 
-  // const handlePromptInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setPromptValue(event.target.value);
-  // };
   return (
     <S.ScriptContainer>
       <S.ScriptMain>
@@ -229,10 +183,6 @@ function RouteComponent() {
               key={subtitle.id}
               id={subtitle.id}
               text={subtitle.text}
-              // font={subtitle.font}
-              // weight={subtitle.weight}
-              // size={subtitle.size}
-              // backgroundStyle={subtitle.backgroundStyle}
               onDelete={deleteSubtitle}
               handleSubTitle={handleSubtitle}
               onSelect={() => setSelectedSubtitle(subtitle)}
@@ -257,81 +207,6 @@ function RouteComponent() {
             onBlur={handleTermValueBlur}
           />
         </S.ScriptTerm>
-        <>
-          {/* <S.PromptSection>
-          <S.LabelSection>
-            <S.SidebarLabel>프롬프트 입력</S.SidebarLabel>
-          </S.LabelSection>
-          <S.PromptInputContainer>
-            <S.PromptInput
-              placeholder="예: 차갑게"
-              value={promptValue}
-              onChange={handlePromptInput}
-            />
-            <S.PromptSubmitButton>적용</S.PromptSubmitButton>
-          </S.PromptInputContainer>
-        </S.PromptSection>
-
-        <S.FontStyleSection>
-          <S.LabelSection>
-            <S.SidebarLabel>자막 스타일</S.SidebarLabel>
-          </S.LabelSection>
-          <S.DropDownContainer>
-            <DropDown
-              dropDownData={fonts}
-              width="100%"
-              onSelect={(data) =>
-                updateSubtitle(subtitles[0]?.id, 'font', data)
-              }
-            />
-            <DropDown
-              dropDownData={weights}
-              width="50%"
-              onSelect={(data) =>
-                updateSubtitle(subtitles[0]?.id, 'weight', data)
-              }
-            />
-            <DropDown
-              dropDownData={sizes}
-              width="45%"
-              onSelect={(data) =>
-                updateSubtitle(subtitles[0]?.id, 'size', data)
-              }
-            />
-          </S.DropDownContainer>
-        </S.FontStyleSection>
-
-        <S.TemplateSection>
-          <S.LabelSection>
-            <S.SidebarLabel>자막 배경 스타일</S.SidebarLabel>
-          </S.LabelSection>
-
-          <S.ScriptTemplateList>
-            <S.ScriptTemplateCard
-              onClick={() => setBackgroundStyle('short')}
-              isSelected={backgroundStyle === 'short'}>
-              <S.ScriptTemplateName>짧은 배경</S.ScriptTemplateName>
-              <ShortScriptTemplate />
-            </S.ScriptTemplateCard>
-
-            <S.ScriptTemplateCard
-              onClick={() => setBackgroundStyle('long')}
-              isSelected={backgroundStyle === 'long'}>
-              <S.ScriptTemplateName>긴 배경</S.ScriptTemplateName>
-
-              <LongScriptTemplate />
-            </S.ScriptTemplateCard>
-
-            <S.ScriptTemplateCard
-              onClick={() => setBackgroundStyle('transparent')}
-              isSelected={backgroundStyle === 'transparent'}>
-              <S.ScriptTemplateName>배경 없음</S.ScriptTemplateName>
-
-              <TransparentBackground />
-            </S.ScriptTemplateCard>
-          </S.ScriptTemplateList>
-        </S.TemplateSection> */}
-        </>
       </S.ScriptToolbar>
     </S.ScriptContainer>
   );
@@ -401,18 +276,7 @@ const S = {
     padding: ${theme.spacing.sm} ${theme.spacing.md};
     z-index: 2;
   `,
-  // VideoInnerScript: styled.span<{ font: string; weight: string; size: string }>`
-  //   font-family: ${(props) => props.font};
-  //   font-weight: ${(props) =>
-  //     props.weight === '얇게'
-  //       ? theme.fontWeights.light
-  //       : props.weight === '중간'
-  //         ? theme.fontWeights.medium
-  //         : theme.fontWeights.bold};
-  //   font-size: ${(props) => props.size};
-  //   color: ${theme.colors.white};
-  //   text-align: center;
-  // `,
+
   ScriptToolbar: styled.div`
     background-color: ${theme.colors.white};
     width: 30%;
@@ -435,53 +299,7 @@ const S = {
     flex-direction: column;
     gap: ${theme.spacing.sm};
   `,
-  // PromptSection: styled.div`
-  //   width: 90%;
-  //   display: flex;
-  //   flex-direction: column;
-  // `,
-  // PromptInputContainer: styled.div`
-  //   width: 100%;
-  //   display: flex;
-  //   justify-content: center;
-  //   gap: ${theme.spacing.sm};
-  // `,
-  // PromptInput: styled.input`
-  //   width: 90%;
-  //   border: 1px solid ${theme.colors.border1};
-  //   border-radius: ${theme.radius.small};
-  //   padding: ${theme.spacing.sm};
-  //   &: focus {
-  //     outline: 2px solid ${theme.colors.secondary1};
-  //   }
-  // `,
-  // PromptSubmitButton: styled.button`
-  //   width: 10%;
-  //   min-width: 50px;
-  //   border: 1px solid ${theme.colors.border1};
-  //   border-radius: ${theme.radius.small};
-  //   padding: ${theme.spacing.sm};
-  //   font-weight: ${theme.fontWeights.medium};
-  // `,
-  // FontStyleSection: styled.div`
-  //   width: 90%;
-  //   display: flex;
-  //   flex-direction: column;
-  //   gap: ${theme.spacing.sm};
-  // `,
-  // DropDownContainer: styled.div`
-  //   display: flex;
-  //   flex-wrap: wrap;
-  //   gap: 5%;
-  //   row-gap: ${theme.spacing.sm};
-  // `,
-  // TemplateSection: styled.div`
-  //   width: 90%;
-  //   height: 100%;
-  //   display: flex;
-  //   flex-direction: column;
-  //   gap: ${theme.spacing.sm};
-  // `,
+
   LabelSection: styled.div`
     display: flex;
     align-items: center;
@@ -491,36 +309,4 @@ const S = {
     font-size: ${theme.fontSizes.fz20};
     font-weight: ${theme.fontWeights.medium};
   `,
-  // ScriptTemplateList: styled.div`
-  //   width: 100%;
-  //   max-height: 100%;
-  //   display: flex;
-  //   flex-direction: column;
-  //   padding: ${theme.spacing.sm};
-  //   gap: ${theme.spacing.sm};
-  //   overflow-x: hidden;
-  //   &::-webkit-scrollbar {
-  //     display: none;
-  //   }
-  // `,
-  // ScriptTemplateCard: styled.div<{ isSelected: boolean }>`
-  //   display: flex;
-  //   align-items: center;
-  //   justify-content: space-between;
-  //   padding: ${theme.spacing.sm};
-  //   width: 100%;
-  //   height: 100%;
-  //   border: 2px solid
-  //     ${(props) =>
-  //       props.isSelected ? theme.colors.secondary1 : theme.colors.border1};
-  //   border-radius: ${theme.radius.medium};
-  //   background-color: ${(props) =>
-  //     props.isSelected ? theme.colors.background1 : 'transparent'};
-  //   svg {
-  //     width: 115px;
-  //   }
-  // `,
-  // ScriptTemplateName: styled.span`
-  //   font-size: ${theme.fontSizes.fz14};
-  // `,
 };
